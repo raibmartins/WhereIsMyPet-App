68c421ecf9c1c718b8ae07787e9ec9c62bf0f97a
BETHA\rai.martins <raibmartins@betha.com.br>
2024-04-13 14:50:10 -0300

    Mudando layout telas de login/registrar

--- src/core/auth/Login copy.tsx
+++ src/core/auth/Login copy.tsx
@@ -6,28 +6,29 @@ import { Input } from "../../components/Input";
 
 export default function Login({ navigation }) {
 
-    const { height, width } = Dimensions.get('window');
+    const {height, width} = Dimensions.get('window');
 
     return (
-        <KeyboardAvoidingView h={height} behavior="padding" p='10'>
+        <KeyboardAvoidingView h={height} w={width} behavior="padding" p='10'>
             <Center h='100%'>
-                <Center shadow={5} borderRadius={80} borderWidth={1}
+                <Center shadow={5} borderRadius={80} borderWidth={1} 
                     borderColor={'green.wGreenUnesc'} backgroundColor='white' padding={10}>
                     <Logo mb="30" />
-                    <Box>
+                    <Box >
                         <FormControl>
                             <Input label="E-mail" />
                             <Input label="Senha" />
-                            <Button
-                                display={'block'}
-                                bg='green.sGreenUnesc'
-                                mt='10'
-                                borderRadius='lg'
-                                shadow='3'
-                                onPress={() => navigation.navigate('MainPage')}>
-                                <Texto color={'white'}>Entrar</Texto>
-                            </Button>
                         </FormControl>
+                    </Box>
+                    <Box alignItems={'center'}>
+                        <Button w='100%' 
+                            bg='green.sGreenUnesc'
+                            mt='10'
+                            borderRadius='lg'
+                            shadow='3'
+                            onPress={() => navigation.navigate('MainPage')}>
+                            Entrar
+                        </Button>
                     </Box>
                     <Box w='100%' flexDirection='row' justifyContent='center' mt='10'>
                         <Texto bold={false}>
--- src/core/auth/Register.tsx
+++ src/core/auth/Register.tsx
@@ -1,36 +1,44 @@
-import {  Box, Button, Center, FormControl, KeyboardAvoidingView} from "native-base";
+import { Box, Button, Center, FormControl, KeyboardAvoidingView } from "native-base";
 import { Logo } from "../../components/SystemLogo";
-import { TouchableOpacity } from "react-native";
+import { Dimensions, TouchableOpacity } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
 import { Input } from "../../components/Input";
 import { Texto } from "../../components/Texto";
 
 export default function Register({ navigation }) {
+
+
+    const { height, width } = Dimensions.get('window');
+
+
     return (
         <>
-             <Box w='100%' flexDirection='row' pl={5} pt='10' backgroundColor='white'>
+            <Box h={'10%'} flexDirection='row' pl={5} pt='15%'>
                 <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                    <MaterialIcons name='arrow-back' size={30} color='#64BC52'/>
+                    <MaterialIcons name='arrow-back' size={30} color='green.sGreenUnesc' />
                 </TouchableOpacity>
             </Box>
-            <KeyboardAvoidingView behavior="padding" pt={10} pb='10' pl={10} pr={10} backgroundColor='white'>
-                <Center>
-                    <Logo mb="50"/>
-                    <Box>
-                        <FormControl>
+            <KeyboardAvoidingView h={'70%'} behavior="padding" p='10'>
+                <Center h='100%'>
+                    <Center shadow={5} borderRadius={80} borderWidth={1}
+                        borderColor={'green.wGreenUnesc'} backgroundColor='white' padding={10}>
+                        <Logo mb="30" />
+                        <Box>
+
                             <FormControl>
-                                <Input label="Nome"/>
-                                <Input label="E-mail"/>
-                                <Input label="Senha" type="password"/>
-                                <Input label="Repita sua senha" type="password"/>
+                                <Input label="Nome" />
+                                <Input label="E-mail" />
+                                <Input label="Senha" type="password" />
+                                <Input label="Repita sua senha" type="password" />
+                                <Button bg='green.sGreenUnesc' mt='10' borderRadius='lg' shadow='3' onPress={() => navigation.navigate('MainPage')}>
+                                    <Texto color={'white'}>
+                                        Registrar-se
+                                    </Texto>
+                                </Button>
                             </FormControl>
-                        </FormControl>
-                    </Box>
-                    <Button w='100%' bg='green.sGreenUnesc' mt='10' borderRadius='lg' shadow='3' onPress={() => navigation.navigate('MainPage')}>
-                        <Texto color={'white'}>
-                            Registrar-se
-                        </Texto>
-                    </Button>
+                        </Box>
+
+                    </Center>
                 </Center>
             </KeyboardAvoidingView>
         </>
